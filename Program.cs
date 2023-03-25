4954aebe520044acd1a3229e797c174851869908
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-03-25 18:43:49 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Um progrrama que exiba o primeiro caractere digitado, ignorando espaços em branco.
+
+String Texto;
+
+Console.Write("Digite algo que...: ");
+Texto = Console.ReadLine()!;
+
+Console.WriteLine(Texto.Trim().Substring(0, 1));
+
